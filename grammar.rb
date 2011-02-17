bad2f7516b95c7e348609176d552735a882ef34e
Stephen Shaw <stephen@stephen-shaw.net>
2011-02-17 22:31:26 +0000

    Some messy documentation

--- grammar.rb
+++ grammar.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-require 'Set'
 
 
 class Array
@@ -20,6 +19,10 @@ class String
   def non_terminal?
     not terminal?
   end
+
+  def empty_string?
+    self == '[]'
+  end
 end
 
 
@@ -30,14 +33,25 @@ class Grammar
   @@epsilon = ''
 
   def self.parse_rule(rule_string, delim=@@default_delim)
-    #TODO allow BNF-style rules
+    # TODO allow BNF-style rules
+    # FIXME nasty things will happen if invalid DCG-type rules are provided
+    rule_string.gsub!(/ /, '');
     head, daughters = rule_string.split(delim)
-    [head.split(' '), daughters.split(' ')]
+    [head.split(','), daughters.split(',')]
   end
 
 
+  # Construct a new Grammar from the supplied rules.
+  #
+  # Rules may be an Array of Arrays (corresponding to heads and daughters of
+  # rules), an Array of DCG-like strings, or a hash mapping head symbols to
+  # possible daughter productions.
   def initialize(rules)
-    @rules = (rules.kind_of? Hash)? rules : {};
+
+    # Allow a previously-generated hash to be dropped into place
+    #FIXME validate the hash?
+    @rules = rules if rules.kind_of? Hash
+    @rules ||= {}
 
     rules.each do |rule|
       add_rule(rule)
@@ -45,7 +59,9 @@ class Grammar
 
   end
 
-
+  # Add a rule to this Grammar
+  # If a DCG-style rule string is supplied, it is tokenized into heads and
+  # daughters.
   def add_rule(rule, dest=@rules)
     rule = self.class.parse_rule(rule) if rule.kind_of? String
 
@@ -57,6 +73,14 @@ class Grammar
   end
 
 
+  # Determine whether or not this Grammar is context-free.
+  #
+  # A grammar is context-freee if and only if all of its rules are of the form 
+  #
+  # A --> w
+  #
+  # Where A is a single non-terminal, and w is an arbitrary sequence of
+  # possibly-empty terminals or non-terminals.
   def context_free?
     self.each do |rule|
       return false if rule.head.length > 1 or rule.head[0].terminal? 
@@ -90,39 +114,80 @@ class Grammar
     true
   end
 
-
+  # Determine whether or not this grammar is strictly regular.
+  #
+  # A grammar is strictly regular if and only if it is left-regular or
+  # right-regular, but not both.
   def strictly_regular?
     return (left_regular? ^ right_regular?)
   end
 
 
+  # Determine whether or not this grammar is regular.
+  # A grammar is regular if and only if it is left-regular or right-regular, or
+  # noth.
   def regular?
     return (left_regular? or right_regular?)
   end
 
-  
-  alias :cnf :chomsky_normal_form
+  # Return a Grammar in Chomsky-normal form which is equivalent to this Grammar.
   def chomsky_normal_form
      
   end
 
-  alias :gnf :greibach_normal_form
+  # Return a Grammar in Greibach-normal form which is equivalent to this Grammar.
   def greibach_normal_form
 
   end
 
-  alias :cnf? :chomsky_normal_form?
+  #FIXME
+  # Determine whether or not this Grammar is in Chomsky-normal form (CNF).
+  #
+  # A Grammar is in CNF if and only if all of its rules are of one of the
+  # following forms:
+  #
+  # (1) A --> B, C
+  # (2) A --> a
+  # (3) S --> []
+  #
+  # where A, B and C are non-terminals, a is a terminal, and [] is the empty
+  # string.
   def chomsky_normal_form?
     if context_free?
       self.each do |rule|
-
+        if rule.daughters.length == 1
+          return false if rule.daughters[0].non_terminal?
+        else
+          return false if rule.daughters.any{|d| d.terminal?}
+        end
       end
+    else
+      false
     end
+    true
   end
 
-  alias :gnf? :chomsky_normal_form?
+  # Determine whether or not this Grammar is in Greibach-normal form (GNF).
+  # 
+  # A Grammar is in GNF if and only if all its rules are of one of the following
+  # forms:
+  #
+  # (1) S --> a, B
+  # (2) S --> []
+  #
+  # where S and B are non-terminals, a is a terminal, and [] is the empty
+  # string.
+  #
   def greibach_normal_form?
-
+    if context_free?
+      self.each do |rule|
+        return false if rule.daughters[0].terminal?
+        return false if rule.daughters.length > 1 and not rule.daughters[0].empty_string?
+      end
+    else
+      false
+    end
+    true
   end
 
 
@@ -145,23 +210,38 @@ class Grammar
   end
 
 
+  # Return a Hash mapping Head symbols to an array of their possible
+  # productions in this grammar
+  #
+  # Order of introduction is not necessarily preserved.
   def to_hash
     the_hash = {}
     the_hash.replace(@rules)
     return the_hash
   end
 
-
+  # Return an Array of Arrays, each corresponding to the Array of head symbols
+  # and Array of daughter symbols of a rule in this Grammar.
+  #
+  # Order of introduction is preserved.
+  #
   def to_array
     the_array = []
     self.map {|rule| the_array.push(rule)}
     return the_array
   end
+
+
+  alias :cnf :chomsky_normal_form
+  alias :cnf? :chomsky_normal_form?
+
+  alias :gnf :greibach_normal_form
+  alias :gnf? :chomsky_normal_form?
 end
 
 
 if __FILE__ == $0
-  rules = ["S S --> [a] [b]", "S --> [c]", "A --> [b] [c]", "D --> [e] [f]"]
+  rules = ["S --> [a],S,[b]", "S -->[c]", "A --> [b],[c]", "D --> [e],[f]"]
   this = Grammar.new(rules)
   puts this
 end
