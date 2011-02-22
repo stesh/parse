0d4c40cb7f7aae4bcefaf0252e80913e0ea9475b
Stephen Shaw <stephen@stephen-shaw.net>
2011-02-22 10:59:33 +0000

    Something of a wishlist for the moment

---
+++ tree.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby -w
+
+
+class Tree < Array
+
+  def self.parse_bracketed(s, delim='()')
+  end
+
+  def initialize(head, daughters)
+  end
+
+  def leaves
+  end
+
+  def flatten
+  end
+
+  def height
+  end
+
+  def subtrees
+  end
+
+  def preorder
+  end
+
+  def postorder
+  end
+
+  def to_grammar
+  end
+
+  def pre_terminals
+  end
+
+  def part_of_speech
+  end
+
+  def chomsky_normal_form
+  end
+
+  def to_s
+  end
+
+  def draw
+  end
+
+  def latex_qtree
+  end
+
+  def penn_bracketed_tree
+  end
+
+  def test
+  end
+
+  alias_method :chomsky_normal_form, :cnf
+
+end
+
+if __FILE__ == $0
+
+end
